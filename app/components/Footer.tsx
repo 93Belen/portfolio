877c53448b8db4f3845cc99fc879cf4fd55e82c0
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-09-12 16:28:44 -0500

    updating content and downloable cv

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(){
     return (
         <section className='bg-black text-white py-[40px] px-[24px] md:px-[80px] flex flex-col gap-[12px] border-t-2 border-t-white mt-7'>
             <h2 className='font-bold text-md'>Belen Garcia Martinez</h2>
-            <p className='text-sm md:max-w-[80%] lg:max-w-[50%] md:text-[1rem]'>Software developer - add one to two sentences about self - maybe an attention grabber. Software developer - add one to two sentences about self </p>
+            <p className='text-sm md:max-w-[80%] lg:max-w-[50%] md:text-[1rem]'>Front-end developer crafting web designs into pixel-perfect reality, one line of code at a time.</p>
             <div className='flex flex-row justify-between'>
                 <div className='flex flex-row gap-[0.75rem]'>
                     <Link href=''><AiFillInstagram className='text-[2rem]'/></Link>
