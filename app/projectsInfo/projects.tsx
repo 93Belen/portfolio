b2604b1cdfaacdc63ac3a0404fd4ee4f2e3a3555
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-09-14 10:47:03 -0500

    updated chips

--- app/projectsInfo/projects.tsx
+++ app/projectsInfo/projects.tsx
@@ -3,13 +3,13 @@ export const projectsInfo = [
         name: 'Jay\'s Construction',
         description: "Developed Jay's Construction website, empowering Jay to not only attract new business but also establish a strong brand presence in the construction industry.",
         link: 'https://www.jaysconstructionkc.com',
-        tech: ['AWS', 'Next.js', 'React', 'TypeScript', 'Tailwind CSS', "Motion Framer", "Zustand", 'Bootstrap']
+        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', "Motion Framer", "Zustand", 'Bootstrap']
     },
     {
         name: 'Wunderlust Guitars',
         description: "Spearheaded the development for a guitar website, ensuring a seamless and engaging online shopping experience for customers worldwide.",
         link: 'https://wunderlust-guitars.vercel.app',
-        tech: ['AWS', 'Next.js', 'React', 'TypeScript', 'Tailwind CSS', 'Bootstrap', 'Motion Framer', 'Magic Link', 'MySQL', 'Prisma', 'Zustand']
+        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', 'Bootstrap', 'Motion Framer', 'Magic Link', 'MySQL', 'Prisma', 'Zustand']
     },
     {
         name: 'Portfolio Belen Garcia Martinez',
