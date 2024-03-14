f2477a04e02252a5680e025b10b27b11aa9a4826
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-03-14 17:07:49 -0500

    added current project

--- app/projectsInfo/projects.tsx
+++ app/projectsInfo/projects.tsx
@@ -9,18 +9,24 @@ export const projectsInfo = [
         name: 'Jay\'s Construction',
         description: "Developed Jay's Construction, a website for a local KC business, alongside an UX designer. I organized, led, developed, and contributed to the design of this website created for an actual client.",
         link: 'https://www.jaysconstructionkc.com',
-        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', "Motion Framer", "Zustand", 'React-Bootstrap']
+        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', "Framer Motion", "Zustand", 'React-Bootstrap']
     },
     {
         name: 'Wunderlust Guitars',
         description: "I led a team of two UX designers, assisted with design tasks, and independently developed this e-commerce website featuring authentic sign-in and checkout functionalities.",
         link: 'https://wunderlust-guitars.vercel.app',
-        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', 'React-Bootstrap', 'Motion Framer', 'Magic Link', 'MySQL', 'Prisma', 'Zustand']
+        tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', 'React-Bootstrap', 'Framer Motion', 'Magic Link', 'MySQL', 'Prisma', 'Zustand']
     },
     {
         name: 'Portfolio Belen Garcia Martinez',
         description: "Crafted a personal portfolio. This highlights my passion for creating digital solutions and demonstrates some of my most recent work.",
         link: 'https://main.d133mxwccc452n.amplifyapp.com',
         tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', 'React-Bootstrap', 'Framer Motion']
-    }
+    },
+    {
+        name: 'Dog Rescue Website',
+        description: "Currently working on a redesign inspired by KC's dog rescue Lucky 13.  Check out the Adopt/Dogs page for a sleek Filter Functionality that makes finding the perfect pup a breeze. Stay tuned for more updates!",
+        link: 'http://lucky13-website-alternative.s3-website.us-east-2.amazonaws.com',
+        tech: ['AWS', 'React', 'JavaScript', 'Tailwind CSS', 'DaisyUI', 'Framer Motion']
+    },
 ]
