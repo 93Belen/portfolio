4f4f570bad06119f624b518edcabc429f1603c2c
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-03-14 15:36:17 -0500

    changed footer again

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(){
     return (
         <section className='bg-black text-white py-[40px] px-[24px] md:px-[80px] flex flex-col gap-[12px] border-t-2 border-t-[#5B5B5B] mt-7 w-screen ml-[-24px] md:ml-[-80px]'>
             <h2 className='font-bold text-md'>Belen Garcia Martinez</h2>
-            <p className='text-sm md:max-w-[80%] lg:max-w-[50%] md:text-[1rem]'>Front-end developer. From Ikea furniture to full-stack web apps... I just love building stuff.</p>
+            <p className='text-sm md:max-w-[80%] lg:max-w-[50%] md:text-[1rem]'>From Ikea furniture to full-stack web apps... I just love building stuff.</p>
             <div className='flex flex-row justify-between'>
                 <div className='flex flex-row gap-[0.75rem]'>
                     <Link target='_blank' href='https://www.instagram.com/evil_belencita/'><AiFillInstagram className='text-[2rem]'/></Link>
