b2adfef567e2aff396fca69feaa6272c8cd25416
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-03-14 15:29:41 -0500

    tweaked projects descriptions

--- app/projectsInfo/projects.tsx
+++ app/projectsInfo/projects.tsx
@@ -1,19 +1,19 @@
 export const projectsInfo = [
     {
         name: 'Food Oasis',
-        description: "I am part of the team that develops, updates, and maintains Food Oasis within the Hack For LA organization.",
+        description: "I am part of the team that develops, updates, tests, and maintains Food Oasis within the Hack For LA organization.",
         link: 'https://foodoasis.la',
         tech: ['React', 'JavaScript', 'Material-UI']
     },
     {
         name: 'Jay\'s Construction',
-        description: "Developed Jay's Construction website, empowering Jay to not only attract new business but also establish a strong brand presence in the construction industry.",
+        description: "Developed Jay's Construction, a website for a local KC business, alongside an UX designer. I organized, led, developed, and contributed to the design of this website created for an actual client.",
         link: 'https://www.jaysconstructionkc.com',
         tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', "Motion Framer", "Zustand", 'React-Bootstrap']
     },
     {
         name: 'Wunderlust Guitars',
-        description: "Spearheaded the development for a guitar website, ensuring a seamless and engaging online shopping experience for customers worldwide.",
+        description: "I led a team of two UX designers, assisted with design tasks, and independently developed this e-commerce website featuring authentic sign-in and checkout functionalities.",
         link: 'https://wunderlust-guitars.vercel.app',
         tech: ['AWS', 'Next JS', 'React', 'TypeScript', 'Tailwind CSS', 'React-Bootstrap', 'Motion Framer', 'Magic Link', 'MySQL', 'Prisma', 'Zustand']
     },
